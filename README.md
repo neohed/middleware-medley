8929b4301ae0d9f2e7611203b47254c11d95aa93
neohed <35173474+neohed@users.noreply.github.com>
2021-12-10 17:39:32 +0000

    Router examples

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # middleware-medley
+
 Test for middleware
+
+http://expressjs.com/en/guide/using-middleware.html
--- index.js
+++ index.js
@@ -15,6 +15,55 @@ app.use(morganLogger('dev')); //This logs requested routes to stdout in dev.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function (req, res, next) {
+    console.log('Application middleware. Time:', Date.now())
+    next()
+})
+
+/*
+app.use(function (req, res, next) {
+    console.log('Second handler (does not call next, ends middleware chain) Time:', Date.now())
+})
+ */
+
+const router = express.Router()
+
+router.use(function (req, res, next) {
+    console.log('Router middleware. Time:', Date.now())
+    next()
+})
+
+router.get('/user', function (req, res, next) {
+    console.log('Router middleware with mount path.')
+    next('router')
+})
+
+router.get('/user', function (req, res, next) {
+    console.log('Never called?') // Never called!
+    next()
+})
+
+router.use(function (req, res, next) {
+    console.log('Final router middleware, no mount path!. Time:', Date.now()) // Also never called!
+    next()
+})
+
+app.use('/', router);
+
+const router2 = express.Router()
+
+router2.use(function (req, res, next) {
+    console.log('Second router!. Time:', Date.now()) // Is called!
+    next()
+})
+
+app.use('/', router2);
+
+app.use(function (req, res, next) {
+    console.log('Final application middleware. Time:', Date.now())
+    next()
+})
+
 app.use(routeNotFoundHandler('Snark Hunt! 404'));
 
 const port = 4057;
